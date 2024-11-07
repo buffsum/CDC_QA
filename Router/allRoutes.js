328f8725842168743dbbf37b3876d18617ca47e7
Laurane Chevray <lauranechevray@MacBook-Pro-de-Laurane.local>
2024-11-07 13:19:59 +0100

    j'ai créé les 4 pages 'auth' pour créer un compte, se connecter, gérer son compte et éditer son mdp. c'est un formulaire très simple, à voir si je retravaille le design plus tard. il faudra dans la page 'account.html' remplacer les value correspondant à la personne qui est connectée

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,6 +3,12 @@ import Route from "./Route.js";
 //Définir ici vos routes
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/galerie", "La galerie", "/pages/galerie.html"),
+    new Route("/test", "page test", "/pages/test.html"),
+    new Route("/signin", "Connexion", "/pages/Auth/signin.html"),
+    new Route("/signup", "Inscription", "/pages/Auth/signup.html"),
+    new Route("/account", "Mon compte", "/pages/Auth/account.html"),
+    new Route("/editPassword", "Changement de mot de passe", "/pages/Auth/editPassword.html"),
     // new Route ("/Galerie", "Galerie", "/pages/galerie.html", "/js/galerie.js"),
 ];
 
