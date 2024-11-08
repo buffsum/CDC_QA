6815fbe686fb08f6a56f0ff497d77302624ad899
Laurane Chevray <lauranechevray@MacBook-Pro-de-Laurane.local>
2024-11-08 15:51:58 +0100

    j'ai continué les scripts pour le formulaire d'inscription avec la validation du mdp

--- js/auth/signup.js
+++ js/auth/signup.js
@@ -19,8 +19,9 @@ function validateForm() {
     const nomOk = validateRequired(inputNom);
     const prenomOk = validateRequired(inputPrenom);
     const mailOK = validateEmail(inputMail);
+    const passwordOK = validatePassword(inputPassword);
 
-    if (nomOk && prenomOk && mailOK) {
+    if (nomOk && prenomOk && mailOK && passwordOK) {
         btnValidation.disabled = false;
     }
     else {
@@ -59,6 +60,22 @@ function validateEmail(input) {
         //c'est pas ok
         inputMail.classList.add("is-invalid");
         inputMail.classList.remove("is-valid");
+        return false;
+    }
+}
+
+function validatePassword(input){
+    //Définir mon regex
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_])[A-Za-z\d\W_]{8,}$/;
+    const passwordUser = input.value;
+    if(passwordUser.match(passwordRegex)){
+        input.classList.add("is-valid");
+        input.classList.remove("is-invalid");
+        return true;
+    }
+    else{
+        input.classList.remove("is-valid");
+        input.classList.add("is-invalid");
         return false;
     }
 }
