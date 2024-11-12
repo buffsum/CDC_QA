79d22d5d97acf6eaa6e7d4c0a3217cdd746faf2d
Laurane Chevray <lauranechevray@MacBook-Pro-de-Laurane.local>
2024-11-12 14:47:22 +0100

    après avoir géré la visibilité de certain éléments selon le role, je suis allée gérer l'accès aux pages selon le role, réglant donc une des failles de sécurité du site. dans mon fichier route.js je suis allée ajouter la variable 'this.authorize=authorize'., j'ai ajouté à chaque route dans allRoutes.js les tableaux correspondant (par ex '[''disconnected''] pour signin, c'est à dire que l'on a accés à ces pages que lorsqu'on est decconecté. pour finir j'ai créé la const 'allRolesArray' dans router.js pour gérer vers où je renvoie mes utilisateurs.

--- Router/Route.js
+++ Router/Route.js
@@ -1,8 +1,19 @@
 export default class Route {
-    constructor(url, title, pathHtml, pathJS = "") {
+    constructor(url, title, pathHtml, authorize, pathJS = "") {
       this.url = url;
       this.title = title;
       this.pathHtml = pathHtml;
       this.pathJS = pathJS;
+      this.authorize = authorize; // pour gérer qui peut accéder à la page selon son rôle
     }
 }
+
+/* pour les authorize ce sera un tableau :
+[] -> tout le monde peut y accéder
+["disconnected"] -> seulement les utilisateurs non connectés (login)
+["connected"] -> seulement les utilisateurs connectés
+["admin"] -> seulement les utilisateurs connectés en tant qu'admin
+["client"] -> seulement les utilisateurs connectés en tant que client
+["admin", "client"] -> seulement les utilisateurs connectés en tant qu'admin ou client
+-> donc 5 cas possibles
+*/
--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,15 +2,15 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/galerie", "La galerie", "/pages/galerie.html"),
-    new Route("/test", "page test", "/pages/test.html"),
-    new Route("/signin", "Connexion", "/pages/Auth/signin.html", "/js/auth/signin.js"),
-    new Route("/signup", "Inscription", "/pages/Auth/signup.html", "/js/auth/signup.js"),
-    new Route("/account", "Mon compte", "/pages/Auth/account.html"),
-    new Route("/editPassword", "Changement de mot de passe", "/pages/Auth/editPassword.html"),
-    new Route("/allResa", "Vos réservations", "/pages/CRUD/allResa.html"),
-    new Route("/reserver", "Réserver", "/pages/CRUD/reserver.html"),
+    new Route("/", "Accueil", "/pages/home.html", []),
+    new Route("/galerie", "La galerie", "/pages/galerie.html", []),
+    new Route("/test", "page test", "/pages/test.html", []),
+    new Route("/signin", "Connexion", "/pages/Auth/signin.html", ["disconnected"], "/js/auth/signin.js"),
+    new Route("/signup", "Inscription", "/pages/Auth/signup.html", ["disconnected"], "/js/auth/signup.js"),
+    new Route("/account", "Mon compte", "/pages/Auth/account.html", ["connected"]),
+    new Route("/editPassword", "Changement de mot de passe", "/pages/Auth/editPassword.html", ["client", "admin"]),
+    new Route("/allResa", "Vos réservations", "/pages/CRUD/allResa.html", ["client"]),
+    new Route("/reserver", "Réserver", "/pages/CRUD/reserver.html", ["client"]),
     // new Route ("/Galerie", "Galerie", "/pages/galerie.html", "/js/galerie.js"),
 ];
 
